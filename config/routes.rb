4d2afeb2fbf51039288db23a3c01b210e372813b
viviana rishe <vivianacuri@gmail.com>
2016-10-27 10:52:16 -0700

    adds user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/' => 'users#index'
+  get 'users/' => 'users#create'
+  get 'users/new' => 'users#new', as: :new_user
 end
