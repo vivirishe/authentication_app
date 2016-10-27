ca212f42a697a231850b46a67b585f46bcc4bc34
viviana rishe <vivianacuri@gmail.com>
2016-10-27 11:13:32 -0700

    adds sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
 
-  get 'users/new'
+  root 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/' => 'users#index'
-  get 'users/' => 'users#create'
+  post 'users/' => 'users#create'
   get 'users/new' => 'users#new', as: :new_user
 end
