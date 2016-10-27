5365f004efd020c1d101c330fb6cf037f73dd83a
viviana rishe <vivianacuri@gmail.com>
2016-10-27 11:38:10 -0700

    adds login and logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-
   root 'users#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/' => 'users#index'
   post 'users/' => 'users#create'
   get 'users/new' => 'users#new', as: :new_user
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
