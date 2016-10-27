38250fe34445ae853ac88fb0bae91a2bcdb668d1
viviana rishe <vivianacuri@gmail.com>
2016-10-27 10:57:05 -0700

    creates user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/' => 'users#index'
   get 'users/' => 'users#create'
