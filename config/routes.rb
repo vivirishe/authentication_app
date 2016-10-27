62f3c68be417b574c570a33323d52391d687b2bd
viviana rishe <vivianacuri@gmail.com>
2016-10-27 12:36:56 -0700

    adds users routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   post 'users/' => 'users#create'
   get 'users/new' => 'users#new', as: :new_user
   get 'users/:id' => 'users#show', as: :user
-  
+  get 'users/:id/edit' => 'users#edit', as: :edit_user
+  patch 'users/:id' => 'users#update'
+  delete 'users/:id' => 'users#destroy'
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
